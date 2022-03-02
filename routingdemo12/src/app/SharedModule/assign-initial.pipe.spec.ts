42ac2a38759f08eaaeb34a7230363fb6064b542a
Gopi Vinay <Gopi.Vinay@euromonitor.com>
2022-03-02 09:42:53 +0530

    Unnecessary files deleted

--- routingdemo12/src/app/SharedModule/assign-initial.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AssignInitialPipe } from './assign-initial.pipe';
-
-describe('AssignInitialPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AssignInitialPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
